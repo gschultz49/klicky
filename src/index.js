564c58c7fc715584db7c38a63c98bde6962fc08e
tkanie123 <tk.anie123@gmail.com>
2023-01-05 14:36:07 -0500

    Commit klicky files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import { Klicky } from "./klicky";
 
 Klicky();
+
+//Click event specifically for clicking the linkedin icon
+import { clickFunction } from "./klicky";
+const element = document.getElementById("linkedin");
+element.addEventListener("click", clickFunction);
+
--- src/klicky.ts
+++ src/klicky.ts
@@ -1,3 +1,7 @@
 export const Klicky = () => {
   console.log("klickly ran!");
 };
+
+export const clickFunction = () => {
+  console.log("linkedin was clicked");
+};
