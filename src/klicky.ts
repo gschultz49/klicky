3bbb037da7af2ef7182654106252df83bb60d58e
judykuang <jkuang2724@bths.edu>
2023-01-06 12:49:43 -0500

    Uses eventListener to output clicks to console

--- src/klicky.ts
+++ src/klicky.ts
@@ -1,3 +1,24 @@
 export const Klicky = () => {
   console.log("klickly ran!");
 };
+
+const outer = document.querySelector('.outer');
+
+const capture = {
+  capture : true
+};
+
+const noneCapture = {
+  capture : false 
+};
+
+const once = {
+  once : false 
+};
+
+outer?.addEventListener('click', onceHandler, once);
+
+function onceHandler(event)
+{
+  console.log("You have clicked the page");
+}
