609381552dcb85b7637d4dc358456be29ff39935
Sanjida <you@example.com>
2023-01-06 01:47:05 -0500

    adding sanjidas file

---
+++ new.js
@@ -0,0 +1,5 @@
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
--- src/klicky.ts
+++ src/klicky.ts
@@ -1,3 +1,8 @@
 export const Klicky = () => {
   console.log("klickly ran!");
 };
+
+
+document.getElementById("yellow")?.addEventListener("click", function(){
+alert("click")
+});
