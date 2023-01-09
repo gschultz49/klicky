4fc508b94e366230518f5c003b2be15a813a7d59
Sanjida <you@example.com>
2023-01-09 12:16:48 -0500

    commit sunny R

--- new.js
+++
@@ -1,5 +0,0 @@
-element.addEventListener("click", myFunction);
-
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
-}
--- src/klicky.ts
+++ src/klicky.ts
@@ -2,7 +2,36 @@ export const Klicky = () => {
   console.log("klickly ran!");
 };
 
+document.addEventListener("click", function(){
+  console.log("clicked");
+})
 
-document.getElementById("yellow")?.addEventListener("click", function(){
-alert("click")
-});
+
+function getEventType(event) {
+  const log = document.getElementById('log');
+  log.innerText = `${event.type}\n${log.innerText}`;
+}
+
+
+// Mouse events
+document.addEventListener('mousedown', getEventType, false); // first
+document.addEventListener('mouseup', getEventType, false);   // second
+document.addEventListener('click', getEventType, false);     // third
+
+
+window.addEventListener('keydown', function (e) {
+  document.querySelector('p').innerHTML = `You pressed ${e.key}`;
+}, false);
+
+document.addEventListener("keydown", function(){
+  console.log("key is going down");
+})
+
+
+
+
+
+window.addEventListener('keydown', function (e) {
+  document.querySelector('p').innerHTML = `You pressed ${e.key}`;
+}, false);
+
