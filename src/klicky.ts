67e4edaa58343bb10c4379b3f9690d58415fd434
madisondegrezia <89614960+madisondegrezia@users.noreply.github.com>
2023-01-09 11:05:07 -0500

    fixes warnings that appeared

--- src/klicky.ts
+++ src/klicky.ts
@@ -3,7 +3,7 @@ export const Klicky = (selector: string) => {
   document.addEventListener("click", function (evt) {
     // selector mode
     if (selector) {
-
+      // @ts-ignore
       if (selector === evt.target.localName) {
         console.log("SELECTOR MODE");
         console.log(selector + " tag clicked", evt);
@@ -11,6 +11,7 @@ export const Klicky = (selector: string) => {
 
     } else {    // all mode
       console.log("ALL MODE");
+      // @ts-ignore
       console.log(evt.target.localName + " anything clicked", evt);
     }
   })
