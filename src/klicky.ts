5d40b4f17341f3b968288a115a693cd8455bd1b8
madisondegrezia <89614960+madisondegrezia@users.noreply.github.com>
2023-01-10 18:27:32 -0500

    Add klicky modes (#6)

--- src/klicky.ts
+++ src/klicky.ts
@@ -1,3 +1,22 @@
-export const Klicky = () => {
+export const Klicky = (dataSelector: string) => {
+  document.addEventListener("click", function (evt) {
+    // selector mode
+    if (dataSelector) {
+      const possibleAttr = evt.target.attributes[dataSelector];
+
+      // @ts-ignore
+      if (possibleAttr != null) {
+        console.log("SELECTOR MODE");
+        console.log(dataSelector + " tag clicked", evt);
+      }
+
+    } else {    // all mode
+      console.log("ALL MODE");
+      // @ts-ignore
+      console.log(evt.target.localName + " anything clicked", evt);
+    }
+  })
+
   console.log("klickly ran!");
 };
+
