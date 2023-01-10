e40ff9ece1e4306cce740cd8b5c7212192b626a5
sanjidafairuse <108957021+sanjidafairuse@users.noreply.github.com>
2023-01-09 19:42:17 -0500

    Update klicky.ts

--- src/klicky.ts
+++ src/klicky.ts
@@ -1,3 +1,40 @@
 export const Klicky = () => {
   console.log("klickly ran!");
 };
+
+function getEventType(event) {
+  const log = document.getElementById('log');
+  log.innerText = `${event.type}\n${log.innerText}`;
+}
+
+document.addEventListener('keydown', getEventType, false); 
+console.log('keydown')
+document.addEventListener('keypress', getEventType, false);
+document.addEventListener('keyup', getEventType, false);   
+
+// Mouse events
+document.addEventListener('mousedown', getEventType, false); // first
+document.addEventListener('mouseup', getEventType, false);   // second
+document.addEventListener('click', getEventType, false);     // third
+
+
+//when key presses down the first line changes and prints "you pressed" on console
+//and any key the user presses will appear beside it.
+
+window.addEventListener('keydown', function (e) {
+  document.querySelector('p').innerHTML = `You pressed ${e.key}`;
+}, false);
+
+//when user presses any key it prints the string "key is going down". 
+document.addEventListener("keydown", function(){
+  console.log("key is going down");
+})
+
+
+//Click anywhere to test the altKey property. MouseEvent.altKey can only be read.
+let log = document.querySelector('log');
+document.addEventListener('click', logKey);
+
+function logKey(e) {
+  log.textContent = `The alt key is pressed: ${e.altKey}`;
+}
