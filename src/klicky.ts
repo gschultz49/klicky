42de3772efa48d7e281793882e27f5891779cc46
tkanie123 <tk.anie123@gmail.com>
2023-01-10 14:54:54 -0500

    Minor changes based on feedback

--- src/klicky.ts
+++ src/klicky.ts
@@ -2,10 +2,13 @@ export const Klicky = () => {
   console.log("klickly ran!");
 };
 
+//Click event specifically for clicking the linkedin icon
 export const clickFunction = () => {
-  //Click event specifically for clicking the linkedin icon
   const element = document.getElementById("linkedin");
-  element.addEventListener("click", clickFunction);
+  if(element != null) //prevents element null error
+  {
+    element.addEventListener("click", clickFunction);
+  }
   console.log("linkedin was clicked");
 };
 
