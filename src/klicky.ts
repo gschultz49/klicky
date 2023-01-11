21525180e1f26b5ea0643912c98bc7ef489f7884
Sanjida <you@example.com>
2023-01-10 20:18:31 -0500

    add Mouse event

--- src/klicky.ts
+++ src/klicky.ts
@@ -2,80 +2,30 @@ export const Klicky = () => {
   console.log("klickly ran!");
 };
 
+// Pointer event
 document.addEventListener("click", function(event){
-//   console.log("clicked");
   console.log(event);
 })
 
 
 function getEventType(event) {
 	console.log (event)
-//   const log = document.getElementById('log');
-//   log.innerText = `${event.type}\n${log.innerText}`;
 }
-
+//keyboard event
 document.addEventListener('keydown', getEventType, false); 
 
 
 
+// Mouse Event
+const button = document.querySelector("button")
+button.addEventListener("mousemove", event => {
+  console.log(event)
+})
 
 
-// console.log('keydown')
+const button = document.querySelector("submit")
+button.addEventListener("submit", event => {
+  console.log(event)
+})
 
 
-
-//when key presses down the first line changes and prints "you pressed" on console
-//and any key the user presses will appear beside it.
-
-// window.addEventListener('keydown', function (e) {
-//   document.querySelector('p').innerHTML = `You pressed ${e.key}`;
-// }, false);
-
-//when user presses any key it prints the string "key is going down". 
-// document.addEventListener("keydown", function(){
-//   console.log("key is going down");
-// })
-
-
-// let log = document.querySelector('#log');
-// document.addEventListener('click', logKey);
-
-// function logKey(e) {
-//   log.textContent = `The alt key is pressed: ${e.altKey}`;
-// }
-
-// //using mouseevent to hover over button and print MouseOver and Mouse Event
-
-// 		const something = document.getElementById("clickIt");
-// 		const bye = document.getElementById("hoverPara");
-
-// 		something.addEventListener("click", RespondClick);
-// 		else.addEventListener("mouseover", RespondMouseOver);
-// 		something.addEventListener("mouseout", RespondMouseOut);
-
-// 		function RespondMouseOver() {
-// 			document.getElementById("effect").innerHTML +=
-// 					"MouseOver Event" + "<br>";
-// 		}
-
-// 		function RespondMouseOut() {
-// 			document.getElementById("effect").innerHTML +=
-// 					"MouseOut Event" + "<br>";
-// 		}
-
-// 		function RespondClick() {
-// 			document.getElementById("effect").innerHTML +=
-// 					"Click Event" + "<br>";
-// 		}
-
-// 		document.addEventListener('MouseOut Event" + "<br>', getEventType, false); 
-// 		console.log("hi");
-
-
-
-// 		function rainbow(event) {
-//             if (event.altKey) {
-//                 alert("Alt key is pressed.");
-//             } else {
-//                 alert("Alt key is not pressed.");
-//             }
