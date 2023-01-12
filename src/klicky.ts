2060e4f26b27f3f6b1eaf0ed55fa91889f4513ba
Greg Schultz <gschultz@gschultz-mn1.linkedin.biz>
2023-01-12 14:54:53 -0500

    repair formatting

--- src/klicky.ts
+++ src/klicky.ts
@@ -1,55 +1,52 @@
+function getEventType(event) {
+  console.log(event);
+}
+
 export const Klicky = (dataSelector: string) => {
   document.addEventListener("click", function (evt) {
-  console.log(evt);
-})
+    console.log(evt);
+  });
 
-function getEventType(event) {
-	console.log (event)
-}
-//keyboard event
-document.addEventListener('keydown', getEventType, false); 
+  //keyboard event
+  document.addEventListener("keydown", getEventType, false);
 
-// Mouse Event
-const button = document.querySelector("button")
-button.addEventListener("mousemove", event => {
-  console.log(event)
-})
-    // selector mode
-    if (dataSelector) {
-      const possibleAttr = evt.target.attributes[dataSelector];
+  // Mouse Event
+  const button = document.querySelector("button");
+  button?.addEventListener("mousemove", (event) => {
+    console.log(event);
+  });
+  // selector mode
+  if (dataSelector) {
+    const possibleAttr = evt.target.attributes[dataSelector];
 
-      // @ts-ignore
-      if (possibleAttr != null) {
-        console.log("SELECTOR MODE");
-        console.log(dataSelector + " tag clicked", evt);
-      }
-
-    } else {    // all mode
-      console.log("ALL MODE");
-      // @ts-ignore
-      console.log(evt.target.localName + " anything clicked", evt);
+    // @ts-ignore
+    if (possibleAttr != null) {
+      console.log("SELECTOR MODE");
+      console.log(dataSelector + " tag clicked", evt);
     }
-  })
-
-
+  } else {
+    // all mode
+    console.log("ALL MODE");
+    // @ts-ignore
+    console.log(evt.target.localName + " anything clicked", evt);
+  }
   console.log("klickly ran!");
-
-const rootHtmlBody = document.querySelector('.rootHtmlBody');
-
-const once = {
-  once : false 
 };
 
-let onceHandler = (event) => 
-{
+const rootHtmlBody = document.querySelector(".rootHtmlBody");
+
+const once = {
+  once: false,
+};
+
+let onceHandler = (event) => {
   console.log("You have clicked the page");
   displayClicksAndDate();
   let date = new Date();
   dateTime(date);
-}
+};
 
-let displayClicksAndDate = () => 
-{
+let displayClicksAndDate = () => {
   const para = document.createElement("p");
   para.innerText = "You have clicked the page!";
   document.body.appendChild(para);
@@ -59,12 +56,11 @@ let displayClicksAndDate = () =>
   datetime.innerHTML = dt;
   document.body.appendChild(datetime);
 
-  document.getElementById('date-time').innerHTML = dt;
-}
+  document.getElementById("date-time").innerHTML = dt;
+};
 
-let dateTime = (date:Date) =>
-{
-  const month = date.getMonth()+1;
+let dateTime = (date: Date) => {
+  const month = date.getMonth() + 1;
   const day = date.getDate();
   const year = date.getFullYear();
 
@@ -72,8 +68,9 @@ let dateTime = (date:Date) =>
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
 
-  console.log(`Click event occurred at: ${month}/${day}/${year} ${hours}:${minutes}:${seconds}`);
-}
-
-rootHtmlBody?.addEventListener('click', onceHandler, once);
+  console.log(
+    `Click event occurred at: ${month}/${day}/${year} ${hours}:${minutes}:${seconds}`
+  );
 };
+
+rootHtmlBody?.addEventListener("click", onceHandler, once);
