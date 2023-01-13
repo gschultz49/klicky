e99c8e6244c0d320489b0b682508ecd656663cfd
judykuang <jkuang2724@bths.edu>
2023-01-13 13:22:53 -0500

    Adds a button to store click events in array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import { ButtonClick, Klicky } from "./klicky";
+import { ButtonClick, Klicky, b_click } from "./klicky";
+window.b_click = b_click;
 
 ButtonClick();
-Klicky('data-tracking');
+Klicky();
 
 
