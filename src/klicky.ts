376d4f236325d2552b2f6915f507e1e7ad02b471
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2023-01-13 10:51:25 -0500

    adjusted content for show more section

--- src/klicky.ts
+++ src/klicky.ts
@@ -62,21 +62,3 @@ let dateTime = (date:Date) =>
 
 rootHtmlBody?.addEventListener('click', onceHandler, once);
 };
-
-// Show More 
-
-export const 
-var extend = document.getElementsByClassName("hidemore");
-var i;
-
-for (i = 0; i < extend.length; i++){
-  extend[i].addEventListener("click",function(){
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    }else{
-      content.style.maxHeight=content.scrollHeight + "px";
-    }
-  });
-}
