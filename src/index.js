77793c88a5746c2b10ff7b02948710d40c64e9a9
tkanie123 <tk.anie123@gmail.com>
2023-01-13 17:00:02 -0500

    All show more buttons work

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { ButtonClick, Klicky } from "./klicky";
 ButtonClick();
 Klicky('data-tracking');
 
-const news_button = document.querySelector("#more_button");
+const news_button = document.querySelector("#news_button");
 const news = document.querySelector("#moreNewsSection");
 
 news_button.addEventListener("click", () => {
@@ -16,3 +16,28 @@ news_button.addEventListener("click", () => {
     }
 });
 
+const groups_button = document.querySelector("#groups_button");
+const groups = document.querySelector("#hide_groups");
+
+groups_button.addEventListener("click", () => {
+    if(groups.style.display === "none") {
+        groups.style.display = "block";
+        groups_button.innerHTML = "Show less 🡡";
+    } else {
+        groups.style.display = "none";
+        groups_button.innerHTML = "Show more 🡣";
+    }
+});
+
+const hash_button = document.querySelector("#hashtag_button");
+const hashtags = document.querySelector("#hide_hash");
+
+hash_button.addEventListener("click", () => {
+    if(hashtags.style.display === "none") {
+        hashtags.style.display = "block";
+        hash_button.innerHTML = "Show less 🡡";
+    } else {
+        hashtags.style.display = "none";
+        hash_button.innerHTML = "Show more 🡣";
+    }
+});
