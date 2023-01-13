2c40942dbac2b9b344df9e25d7bf3c39c4bb7d73
tkanie123 <tk.anie123@gmail.com>
2023-01-13 10:54:50 -0500

    Added the JS function for the show more button (news section) to the JS file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-
 import { Klicky } from "./klicky";
 
 Klicky('data-tracking');
 
+const news_button = document.querySelector("#more_button");
+const news = document.querySelector("#moreNewsSection");
 
+news_button.addEventListener("click", () => {
+    if(news.style.display === "none") {
+        news.style.display = "block";
+        news_button.innerHTML = "Show less 🡡";
+    } else {
+        news.style.display = "none";
+        news_button.innerHTML = "Show more 🡣";
+    }
+});
