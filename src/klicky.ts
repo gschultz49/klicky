63e5eb9af2ac5a433516caa2d51e3cfe3c1a018a
judykuang <jkuang2724@bths.edu>
2023-01-17 10:01:38 -0500

    Added modules

--- src/klicky.ts
+++ src/klicky.ts
@@ -131,26 +131,3 @@ textBox.addEventListener('keydown', (event) => output.textContent = `Key board e
   }
 };
 
-anychart.onDocumentReady(function () {
-  // create data
-  var data = [
-    {x: "2010", y: "A", heat: 15},
-    {x: "2011", y: "A", heat: 17},
-    {x: "2012", y: "A", heat: 21},
-    {x: "2010", y: "B", heat: 34},
-    {x: "2011", y: "B", heat: 33},
-    {x: "2012", y: "B", heat: 32},
-    {x: "2010", y: "C", heat: 51},
-    {x: "2011", y: "C", heat: 50},
-    {x: "2012", y: "C", heat: 47}
-  ];
-
-  // create a chart and set the data
-  chart = anychart.heatMap(data);
-
-  // set the container id
-  chart.container("container");
-
-  // initiate drawing the chart
-  chart.draw();
-});
