fe1843496669a38ea84a8503dfcdbc1cb8a4a373
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2023-01-17 09:39:45 -0500

    created database

--- src/index.js
+++ src/index.js
@@ -41,3 +41,27 @@ hash_button.addEventListener("click", () => {
         hash_button.innerHTML = "Show more 🡣";
     }
 });
+
+/* Database */
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCLcorYXXxmSJjn7qDMPK_5m-DiD77m8nk",
+  authDomain: "click-tracking-data.firebaseapp.com",
+  projectId: "click-tracking-data",
+  storageBucket: "click-tracking-data.appspot.com",
+  messagingSenderId: "940131090453",
+  appId: "1:940131090453:web:1294d87473411f56170e1c",
+  measurementId: "G-S07EHVYYHZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
