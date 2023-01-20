7393f9f3ad6ce11b256a72dba0445a65ff3a19d4
Judy Kuang <121958473+judykuang@users.noreply.github.com>
2023-01-20 12:23:13 -0500

    Changes var -> let (#47)

--- src/map.js
+++ src/map.js
@@ -1,18 +1,8 @@
-<html>
-
-<head>
-    <title>Heat Map</title>
-    <script src="https://cdn.anychart.com/releases/8.11.0/js/anychart-core.min.js"></script>
-    <script src="https://cdn.anychart.com/releases/8.11.0/js/anychart-heatmap.min.js"></script>
-</head>
-
-<body>
-    <div id="container"></div>
-    <script>
+<script>
         anychart.onDocumentReady(function () {
 
             // create data
-            let data = [
+            var data = [
                 { x: "Left Sidebar", y: "1", heat: 20 },
                 { x: "LinkedIn Feed (Left)", y: "1", heat: 17 },
                 { x: "LinkedIn Feed (Right)", y: "1", heat: 21 },
@@ -27,7 +17,7 @@
                 { x: "Right Sidebar", y: "3", heat: 12 }
                 ];
             // create a chart and set the data
-            let chart = anychart.heatMap(data);
+            var chart = anychart.heatMap(data);
 
             // set the chart title
             chart.title("This heat map shows which parts of the page have been interacted/clicked on most");
@@ -39,10 +29,3 @@
             chart.draw();
         });
     </script>
-    <script src="./src/index.js"></script>
-    <div style="text-align: center;">
-        <img src="legend.jpg">
-    </div>
-    
-</body>
-</html>
